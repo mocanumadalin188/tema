01f9f1ea7a8a3f44643e546fa5feb63f232af18e
verdictus <verdictus@users.noreply.github.com>
2021-05-10 22:36:14 +0300

    ss

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8081
 
 app.get('/jokes', function (req, res) {
   res.sendFile('jokes.html', { root: '.' })
